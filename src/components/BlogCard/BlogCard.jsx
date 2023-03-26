357107c8c8f2d7e6aefa88716add919df4cf6f52
AlexanderShevchenko112 <aleks.shevchenko.2013@gmail.com>
2023-03-26 19:03:09 +0300

    task 1 is completed

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,54 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import {
+  Card,
+  CardHeader,
+  CardPoster,
+  CardBody,
+  Tag,
+  CardTitle,
+  CardText,
+  CardFooter,
+  UserBox,
+  Avatar,
+  UserInfo,
+  UserName,
+  Date,
+} from './BlogCard.styled';
+import { formatDateToNow } from 'helpers/formatDate';
+import PropTypes from 'prop-types';
+export const BlogCard = ({
+  articles: { name, postedAt, poster, tag, title, description, avatar },
+}) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardPoster src={poster} alt={tag} />
+      </CardHeader>
+      <CardBody>
+        <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+        <CardText>{description}</CardText>
+      </CardBody>
+      <CardFooter>
+        <UserBox>
+          <Avatar src={avatar} alt={name} />
+          <UserInfo>
+            <UserName>{name}</UserName>
+            <Date>{formatDateToNow(postedAt)}</Date>
+          </UserInfo>
+        </UserBox>
+      </CardFooter>
+    </Card>
+  );
+};
+
+BlogCard.propTypes = {
+  articles: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    postedAt: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+  }).isRequired,
 };
